a40f1666cec820b28a707fc9e6fe5b6ef2e7bd4d
nikshansherov <nikshansherov@yandex.ru>
2022-10-03 08:41:13 +0300

    initial Home_work_1

---
+++ Home_work_1/task_02.py
@@ -0,0 +1,8 @@
+# Проверяет истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+print('X Y Z')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if not(x or y or z) == (not x) and (not y) and (not z):
+                print(x, y, z)
